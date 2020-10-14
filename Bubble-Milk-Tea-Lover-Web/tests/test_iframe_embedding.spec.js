a92b1f5cd0d59ebd77eed44f8cf0278d9410d5f7
JLElsa <hlee3288@uni.sydney.edu.au>
2020-10-14 21:55:40 +1100

    Create test_iframe_embedding.spec.js

---
+++ Bubble-Milk-Tea-Lover-Web/tests/test_iframe_embedding.spec.js
@@ -0,0 +1,27 @@
+/// <reference types="cypress" />
+describe("Test iframe embedding", () => {
+  context("Window", () => {
+    beforeEach(() => {
+      cy.visit(
+        "https://htmlpreview.github.io/?https://github.com/JLElsa/Jas/blob/master/Bubble-Milk-Tea-Lover-Web/home.html"
+      );
+    });
+
+    it("Check document object character code", () => {
+      cy.document().should("have.property", "charset").and("eq", "UTF-8");
+    });
+
+    it("Check title", () => {
+      cy.title().should("include", "Home");
+    });
+
+    it("Check youtube embedding", () => {
+      cy.get("iframe").should("have.class", "youtube");
+      cy.xpath("//*[@class='youtube']").then(($ele) => {
+        var ifele = $ele.contents().find(".ytp-large-play-button");
+        // prompt($ele);
+        // cy.wrap(ifele).click();
+      });
+    });
+  });
+});
